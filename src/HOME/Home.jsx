fc28c6a5d4e883c549f1dc1012479f53294d4c56
afrid0126 <afridk2601@gmail.com>
2025-03-14 13:02:40 +0530

    adding the about ted

--- src/HOME/Home.jsx
+++ src/HOME/Home.jsx
@@ -30,15 +30,51 @@ const Home = () => {
     border: "none",
     cursor: "pointer",
     position:"relative",
+    left:"45%",
   };
+  const AboutTedStyle=
+  {
+color: "#FFF",
+fontFamily: "Helvetica",
+fontSize:"14px",
+fontStyle:"normal",
+fontWeight:"300",
+lineHeight:"125.477%", 
+width:"1191px",
+height:"107px",
+flexShrink: "0",
+border:"1px solid red",
+borderRadius:"18px",
+backgroundColor:"black",
+alignItems:"center",
+  }
+  const AboutHeadStyle={
+    display:"flex",
+    flexDirection:"column",
+  }
 
   return (
     <div className="home" style={{ backgroundColor: "black" }}>
       <div className="video_head" style={videoHeadStyle}>
         <Header />
-        <video src={intro} autoPlay loop muted style={videoStyle} />
+        <video style={videoStyle} autoPlay loop muted>
+  <source src={intro} type="video/mp4" />
+  Your browser does not support the video tag.
+</video>
       </div>
       <button type="submit" style={buttonStyle}>REGISTER</button>
+      <div>
+        <div style={AboutHeadStyle}>
+          <h2 style={{color:"white"}}>About </h2>
+          <h2 style={{color:"red"}}>TED</h2>
+        </div>
+        <div>
+        <p style={AboutTedStyle}>
+        TED(Technology, Entertainment, Design) is a non-profit media organization that posts international talks online for free distribution under the slogan "Ideas change everything". TED covers almost all topics – from science to business to global issues – in more than 100 languages. It hosts talks that shares unique ideas about almost everything under the sun, from business ventures to cooking techniques. A suite of short, carefully prepared talks, demonstrations and performances that are idea-focused, and cover a wide range of subjects to foster learning, inspiration and wo`nder – and provoke conversations that matter.  
+        </p>
+          </div>
+               
+      </div>
     </div>
   );
 };
